efa5e0f71b6ecb564b11e2700c63213ded090ff4
Ryo Yamagiwa <ryo.yamagiwa0250@gmail.com>
2026-07-20 20:00:59 +0900

    viewModelFactoryを実装、連打制御を導入

--- app/src/main/java/ryo/myappcompany/fixingaflawedweatherforecastappkotlin/ui/WeatherViewModel.kt
+++ app/src/main/java/ryo/myappcompany/fixingaflawedweatherforecastappkotlin/ui/WeatherViewModel.kt
@@ -1,11 +1,18 @@
 package ryo.myappcompany.fixingaflawedweatherforecastappkotlin.ui
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
+import ryo.myappcompany.fixingaflawedweatherforecastappkotlin.WeatherApplication
 import ryo.myappcompany.fixingaflawedweatherforecastappkotlin.repository.WeatherRepository
 
 /**
@@ -22,6 +29,16 @@ class WeatherViewModel(
     val weatherUiState: LiveData<WeatherUiState>
         get() = _weatherUiState
 
+    companion object {
+        @JvmField
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val app = this[APPLICATION_KEY] as WeatherApplication
+                WeatherViewModel(app.weatherRepository)
+            }
+        }
+    }
+
     /**
      * 天気情報取得
      *
@@ -30,7 +47,14 @@ class WeatherViewModel(
      * @return 天気情報
      */
     fun fetchWeatherData(cityId: String) {
+
+        // 多重実行防止
+        if (weatherUiState.value == WeatherUiState.Loading) {
+            return
+        }
+
         viewModelScope.launch {
+            Log.i(TAG, "start fetchWeatherData!")
             _weatherUiState.value = WeatherUiState.Loading
 
             try {
