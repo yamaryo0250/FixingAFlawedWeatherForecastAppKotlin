f3efd3d7c0e9ce81b7a422bebe00564fb949dff2
Ryo Yamagiwa <ryo.yamagiwa0250@gmail.com>
2026-07-20 20:06:14 +0900

    取得処理は、Dispatchers.IOを使用するように修正

--- app/src/main/java/ryo/myappcompany/fixingaflawedweatherforecastappkotlin/WeatherClient.kt
+++ app/src/main/java/ryo/myappcompany/fixingaflawedweatherforecastappkotlin/WeatherClient.kt
@@ -2,18 +2,23 @@ package ryo.myappcompany.fixingaflawedweatherforecastappkotlin
 
 import android.content.Context
 import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class WeatherClient(private var context: Context?) {
 
     /**
      * 指定した都市コードの天気予報データを取得する
      */
-    fun fetchWeatherData(cityId: String): String {
+    suspend fun fetchWeatherData(cityId: String): String {
         Log.d("WeatherClient", "Fetching weather data for $cityId...")
 
         // 実際のネットワーク通信はメインスレッドで実行するとNetworkOnMainThreadExceptionで
         // クラッシュするため、ここでは重い同期通信処理をThread.sleepでシミュレートしています。
-        Thread.sleep(5000)
+        withContext(Dispatchers.IO) {
+            Log.d("WeatherClient", "Fetching wait...")
+            Thread.sleep(5000)
+        }
 
         // APIから返却された想定のJSON文字列
         val response = """
